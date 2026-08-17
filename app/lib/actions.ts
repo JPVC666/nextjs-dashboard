b0bba4ee411e6cc2bf6edbf675198fdc8f2d3ae7
JPVC666 <r0840634@student.thomasmore.be>
2026-08-17 17:40:00 +0200

    chapter 11 and 12 done mutating data and handling errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -28,11 +28,19 @@ export async function createInvoice(formData: FormData) {
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
-    // inserting the data into your database p2
-    await sql`
+    try {
+        // inserting the data into your database p2
+        await sql`
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
+    } catch (error) {
+        //We'll also log the error to the console for now
+        console.error(error);
+        return {
+            message: 'Database Error: Failed to create invoice',
+        }
+    }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
@@ -46,12 +54,20 @@ export async function updateInvoice(id: string, formData: FormData) {
     });
 
     const amountInCents = amount * 100;
-
-    await sql`
-        UPDATE invoices
-        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-        WHERE id = ${id}
-    `;
+    try {
+        // inserting the data into your database p2
+        await sql`
+            UPDATE invoices
+            SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+            WHERE id = ${id}
+        `;
+    } catch (error) {
+        //We'll also log the error to the console for now
+        console.error(error);
+        return {
+            message: 'Database Error: Failed to Update invoice',
+        }
+    }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
